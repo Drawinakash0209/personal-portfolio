48a7947ef2eeffeb41f781f8bb124db3a4204fa1
Drawinakash0209 <drawinakash77@gmail.com>
2025-06-16 21:47:04 +0530

    Created 3D model and updated hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,13 +1,33 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import Button from '../components/Button.jsx';
+import HeroExperiance from '../components/HeroModels/HeroExperiance.jsx';
 import { words } from '../constants/index.js';
 
 const Hero = () => {
+
+    useGSAP(() =>{
+        gsap.fromTo('.hero-text h1', 
+            {
+                y:50,
+                opacity: 0
+            },
+            {
+                y:0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 5,
+                ease: 'power2.inOut'
+            },
+        )
+    })
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
                 <img src="/images/bg.png" alt="Background"/>
             </div> 
-            <div className="Hero-layout">
+
+            <div className="hero-layout">
                 {/* LEFT: Hero content goes here */}
                 <header className="flex flex-col justify-center md:w-full w:screen md:px-20 px-5">
                     <div className="flex flex-col gap-7">
@@ -43,6 +63,13 @@ const Hero = () => {
                             </Button>
                     </div>
                 </header>
+
+                {/* RIGHT: Hero image goes here */}
+                <figure>
+                    <div className="hero-3d-layout">
+                        <HeroExperiance />
+                    </div>
+                </figure>
             </div>     
         </section>
     );
